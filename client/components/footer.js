92d9604e6ea68a0f89114d2117e496eb063dcdc6
Dyane Ramos <dyane_r@yahoo.es>
2019-07-02 16:44:39 -0400

    footer with commands instructions

---
+++ client/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer-parent">
+      <div>H: moves to left</div>
+      <div>L: moves to right</div>
+      <div>J: moves up</div>
+      <div>K: moves down</div>
+      <div>esc: enables VIM commands</div>
+      <div>I: enables regular commands</div>
+    </div>
+  )
+}
+
+export default Footer
