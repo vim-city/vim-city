38e77628b929a4c1bb2ac9dd28b654c8f0b5ed99
Dyane Ramos <dyane_r@yahoo.es>
2019-07-05 16:25:46 -0400

    welcome message style

--- client/components/modal.js
+++ client/components/modal.js
@@ -39,19 +39,32 @@ class Modal extends React.Component {
               <div
                 className={this.openModal ? 'modal modal--is-open' : 'modal'}
               >
-                <button type="button" onClick={this.closeModal}>
+                <button
+                  className="welcome-button"
+                  type="button"
+                  onClick={this.closeModal}
+                >
                   Start Playing!
                 </button>
 
-                <p>
-                  Vin is visiting for vacation and is having a very hard time
-                  getting around. Help Vin out by following these Vim
-                  commands.Vim has two mode . 1. Insert mode (Where you can just
-                  type like normal text editor. Press i for insert mode) 2.
-                  Command mode (Where you give commands to the editor to get
-                  things done . Press ESC for command mode) THESE INSTRUCTIONS
-                  CAN BE FOUND AT THE BOTTOM OF THE SCREEN
-                </p>
+                <div className="welcome-text">
+                  <p>
+                    Vin is visiting for vacation and is having a very hard time
+                    getting around. Help Vin out by following these Vim
+                    commands.Vim has two modes:
+                  </p>
+                  <p>
+                    1. Insert mode: where you can just type like normal text
+                    editor. (Press i for insert mode)
+                  </p>
+                  <p>
+                    2. Command mode: where you give commands to the editor to
+                    get things done. (Press ESC for command mode)
+                  </p>
+                  <strong>
+                    THESE INSTRUCTIONS CAN BE FOUND AT THE BOTTOM OF THE SCREEN
+                  </strong>
+                </div>
               </div>
             </div>
           ) : null}
