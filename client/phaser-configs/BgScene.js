d18fbd6fcf03d9c6c42351107d40b6b4e42f9335
Nikki Bergamini <nikkibergamini@gmail.com>
2019-07-05 07:55:53 -0400

    game design updated

--- client/phaser-configs/BgScene.js
+++ client/phaser-configs/BgScene.js
@@ -9,17 +9,15 @@ export default class BgScene extends Phaser.Scene {
     // Preload Sprites
     // << LOAD SPRITE HERE >>
 
-    this.load.image('sky', 'assets/backgrounds/background.png')
-    // this.load.image('logo', 'assets/backgrounds/fullBlastLogo.png')
+    this.load.image('sky', '/assets/backgrounds/VimCityBGD.png')
   }
 
   create() {
     // Create Sprites
     // << CREATE SPRITE HERE >>
     this.add
-      .image(-160, 0, 'sky')
+      .image(5, 5, 'sky')
       .setOrigin(0)
-      .setScale(0.5)
-    // this.add.image(380, 80, 'logo').setScale(5)
+      .setScale(0.98)
   }
 }
