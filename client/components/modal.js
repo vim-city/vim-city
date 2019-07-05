40bdcc8585c2dbf905d5e282f1b559e69e35bedb
Dyane Ramos <dyane_r@yahoo.es>
2019-07-05 17:01:02 -0400

    small change

--- client/components/modal.js
+++ client/components/modal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {toggleClose} from '../store/modal'
-import Zoom from 'react-reveal/Zoom'
 
 class Modal extends React.Component {
   constructor(props) {
@@ -33,47 +32,44 @@ class Modal extends React.Component {
   }
   render() {
     return (
-      <Zoom left>
-        <div className="buttonContainer">
-          <div>
-            {this.state.isModalOpen === true ? (
-              <div>
-                <div
-                  className={this.openModal ? 'modal modal--is-open' : 'modal'}
+      <div className="buttonContainer">
+        <div>
+          {this.state.isModalOpen === true ? (
+            <div>
+              <div
+                className={this.openModal ? 'modal modal--is-open' : 'modal'}
+              >
+                <button
+                  className="welcome-button"
+                  type="button"
+                  onClick={this.closeModal}
                 >
-                  <button
-                    className="welcome-button"
-                    type="button"
-                    onClick={this.closeModal}
-                  >
-                    Start Playing!
-                  </button>
+                  Start Playing!
+                </button>
 
-                  <div className="welcome-text">
-                    <p>
-                      Vin is visiting for vacation and is having a very hard
-                      time getting around. Help Vin out by following these Vim
-                      commands.Vim has two modes:
-                    </p>
-                    <p>
-                      1. Insert mode: where you can just type like normal text
-                      editor. (Press i for insert mode)
-                    </p>
-                    <p>
-                      2. Command mode: where you give commands to the editor to
-                      get things done. (Press ESC for command mode)
-                    </p>
-                    <b>
-                      THESE INSTRUCTIONS CAN BE FOUND AT THE BOTTOM OF THE
-                      SCREEN
-                    </b>
-                  </div>
+                <div className="welcome-text">
+                  <p>
+                    Vin is visiting for vacation and is having a very hard time
+                    getting around. Help Vin out by following these Vim
+                    commands.Vim has two modes:
+                  </p>
+                  <p>
+                    1. Insert mode: where you can just type like normal text
+                    editor. (Press i for insert mode)
+                  </p>
+                  <p>
+                    2. Command mode: where you give commands to the editor to
+                    get things done. (Press ESC for command mode)
+                  </p>
+                  <b>
+                    THESE INSTRUCTIONS CAN BE FOUND AT THE BOTTOM OF THE SCREEN
+                  </b>
                 </div>
               </div>
-            ) : null}
-          </div>
+            </div>
+          ) : null}
         </div>
-      </Zoom>
+      </div>
     )
   }
 }
