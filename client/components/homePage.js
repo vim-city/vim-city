c992a93627be5b3282da2cf85208ee16c39a9d6d
Taylor Thompson <ttaythompson@gmail.com>
2019-07-07 08:56:41 -0400

    removed loading page styling

--- client/components/homePage.js
+++ client/components/homePage.js
@@ -2,29 +2,16 @@ import React from 'react'
 import GameWindow from './game-window'
 import VimShell from './vim-shell'
 import Footer from './footer'
-import {connect} from 'react-redux'
 import NavBar from './navbar'
 import Dialogue from './dialogue'
 
 import Modal from './modal'
 
-class HomePage extends React.Component {
+export default class HomePage extends React.Component {
   constructor() {
     super()
   }
   render() {
-    if (this.props.loading)
-      return (
-        <div id="clouds">
-          <div className="cloud x1" />
-          <div className="cloud x2" />
-          <div className="cloud x3" />
-          <div className="cloud x4" />
-          <div className="cloud x5" />
-          <div className="cloud x6" />
-          <div className="cloud x7" />
-        </div>
-      )
     return (
       <div className="home-page">
         <div className="vim-navbar">
@@ -51,11 +38,3 @@ class HomePage extends React.Component {
     )
   }
 }
-
-const mapState = state => {
-  return {
-    loading: state.loading
-  }
-}
-
-export default connect(mapState)(HomePage)
